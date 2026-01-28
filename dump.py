2b0c82634a494ea6a8903e2c20c2ce968d6602c8
Jamiras <jamirasmt@gmail.com>
2026-01-28 17:09:38 +0000

    dump menu text

--- dump.py
+++ dump.py
@@ -9,6 +9,9 @@
 
 # dump original text:
 #  ./dump.py --rom loretta.sg --text original_text.txt --charmap original_font.txt
+#  ./dump.py --rom loretta.sg --menu original_menu.txt --charmap original_font.txt
+#  ./dump.py --rom loretta.sg --words original_words.txt --charmap original_font.txt
+
 
 from utils.rom import ROM
 from utils.tiles import TileTable, Format
@@ -22,6 +25,8 @@ parser.add_option('-r', '--rom', action='store', dest='rom_path', help='source R
 parser.add_option('-s', '--font', action='store', dest='font_path', help='dump font to file')
 parser.add_option('-p', '--png', action='store_true', default='False', dest='png', help='dump to PNG file instead of text')
 parser.add_option('-t', '--text', action='store', dest='text_path', help='dump strings to file')
+parser.add_option('-m', '--menu', action='store', dest='menu_path', help='dump menu strings to file')
+parser.add_option('-w', '--words', action='store', dest='word_path', help='dump word strings to file')
 parser.add_option('-c', '--charmap', action='store', dest='charmap_path', help='character map to use when dumping strings')
 
 
@@ -50,3 +55,20 @@ if options.text_path:
 
     charmap = CharMap(options.charmap_path)
     charmap.dump(rom, 0x1C000, 0x17B, options.text_path)
+
+if options.menu_path:
+    if not options.charmap_path:
+        printf("--charmap is required for --menu")
+        exit()
+
+    charmap = CharMap(options.charmap_path)
+    charmap.dump(rom, 0x200F, 0x0E, options.menu_path)
+
+if options.word_path:
+    if not options.charmap_path:
+        printf("--charmap is required for --menu")
+        exit()
+
+    charmap = CharMap(options.charmap_path)
+    # pointer at 204B=2119
+    charmap.dump(rom, 0x2119, 0x5A, options.word_path)
